f3176dbd6852513106e9fced0646dd4460b3a1af
iverty <berezhnoy20025@gmail.com>
2022-06-14 20:19:13 +0300

    done

--- lesson3/route.rb
+++ lesson3/route.rb
@@ -1,39 +1,31 @@
 # frozen_string_literal: true
 
 # Класс Route (Маршрут):
-# Имеет начальную и конечную станцию, а также список промежуточных станций. Начальная и конечная станции указываютсся
-# при создании маршрута, а промежуточные могут добавляться между ними.
-# Может добавлять промежуточную станцию в список
-# Может удалять промежуточную станцию из списка
-# Может выводить список всех станций по-порядку от начальной до конечной
 class Route
-  attr_reader :all_stations
-
+  # Имеет начальную и конечную станцию, а также список промежуточных станций. Начальная и конечная станции указываютсся
+  # при создании маршрута, а промежуточные могут добавляться между ними.
   def initialize(start_station, end_station)
-    # validation
-    start_station = start_station.instance_of?(Station) ? start_station : nil
-    end_station = end_station.instance_of?(Station) ? end_station : nil
-
-    @all_stations = [start_station, end_station]
+    @start_station = start_station.instance_of?(Station) ? start_station : nil
+    @end_station = end_station.instance_of?(Station) ? end_station : nil
+    @intermediate_stations = []
   end
 
+  # Может добавлять промежуточную станцию в список
   def add_intermediate_station(station)
-    if station.instance_of?(Station)
-      @all_stations.insert(-2, station)
-    else
-      puts 'Invalid station'
-    end
+    @intermediate_stations.push(station) if station.instance_of?(Station)
   end
 
+  # Может удалять промежуточную станцию из списка
   def delete_intermediate_station(station)
-    @all_stations.delete(station) ? @all_stations.include?(station) : puts("This station isn't in train's route")
+    @intermediate_stations.delete(station) if @intermediate_stations.include?(station)
   end
 
+  def all_stations
+    [@start_station] + @intermediate_stations + [@end_station]
+  end
+
+  # Может выводить список всех станций по-порядку от начальной до конечной
   def show_all_stations
-    route = []
-    @all_stations.each do |station|
-      route.push(station.name) ? !station.name.nil? : 'Invalid station'
-    end
-    puts route
+    puts all_stations
   end
 end
